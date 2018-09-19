696d88a88f465d48d60b744c5163d8f28662641b
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-09-19 16:12:47 -0300

    Changing username to email field
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>

--- login/users/models.py
+++ login/users/models.py
@@ -1,5 +1,53 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.core import validators
+from django.utils import six, timezone
+
+class AbstractUser(AbstractBaseUser, PermissionsMixin):
+
+    username = models.EmailField(_('email'), unique=True,
+        error_messages={
+            'unique': _("Esse email já está cadastrado."),
+        })
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
+    last_name = models.CharField(_('last name'), max_length=30, blank=True)
+    email = models.CharField(_('email address'), max_length=30, blank=True)
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+        abstract = True
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email], **kwargs)
+
 
 class CustomUser(AbstractUser):
     name = models.CharField(blank=True, max_length=255)
