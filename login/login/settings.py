46b967616056220caa2f5037a2dc8fe461500c3a
shayanealcantara <shayaneliebe@gmail.com>
2018-09-19 15:24:22 -0300

    Adjustments settings.py
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>

--- login/login/settings.py
+++ login/login/settings.py
@@ -101,6 +101,9 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 JWT_AUTH = {
