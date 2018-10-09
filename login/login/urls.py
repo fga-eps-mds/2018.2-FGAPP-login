270084dd5deabbecfaf726cad8ac156421529121
Andre Lucas <andrelucax@gmail.com>
2018-10-08 22:44:52 -0300

    Adding basic django url for password change
    Co-authored-by: Andre Pinto <andrelucax@gmail.com>
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>

--- login/login/urls.py
+++ login/login/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls import url
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,4 +12,5 @@ urlpatterns = [
     url(r'^api/auth-token/', obtain_jwt_token),
     url(r'^api/token-refresh/', refresh_jwt_token),
     url(r'^api/token-verify/', verify_jwt_token),
+    url('^', include('django.contrib.auth.urls')),
 ]
