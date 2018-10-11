e6b666c94cb35cfb73af4a242f06412a1175b088
Andre Lucas <andrelucax@gmail.com>
2018-10-11 12:24:26 -0300

    Created real path with .env
    Co-authored-by: Andre Pinto <andrelucax@gmail.com>
    Co-authored-by: Gustavo Marques <gustavomarques.gml@gmail.com>

--- login/login/settings/common.py
+++ login/login/settings/common.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -142,3 +143,5 @@ APPEND_SLASH=False
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_DEFAULT_DOMAIN = config('LOGIN_DOMAIN', default='http://localhost:8000')
--- login/login/urls.py
+++ login/login/urls.py
@@ -5,6 +5,8 @@ from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 from django.contrib.auth import views as auth_views
+from django.contrib.sites.models import Site
+from login.settings.development import LOGIN_DEFAULT_DOMAIN
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,3 +16,9 @@ urlpatterns = [
     url(r'^api/token-verify/', verify_jwt_token),
     url('^', include('django.contrib.auth.urls')),
 ]
+
+site = Site.objects.get(id=1)
+site.name = LOGIN_DEFAULT_DOMAIN
+site.domain = LOGIN_DEFAULT_DOMAIN
+site.save()
+current_domain = LOGIN_DEFAULT_DOMAIN
