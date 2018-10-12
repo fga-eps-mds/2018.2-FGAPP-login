c3c0a25c8a6108eaffaec52ddf03a5fb6d53016b
Leonardo Medeiros <leonardomedeiros.6@gmail.com>
2018-10-11 22:27:40 -0300

    Adding get_name function. Removing need for authentication for requests
    Co-authored-by: Shayane Alcantara <shayaneliebe@gmail.com>
    Co-authored-by: Leonardo Medeiros <leonardomedeiros.6@gmail.com>

--- login/login/settings/common.py
+++ login/login/settings/common.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'login.wsgi.application'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        # 'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
--- login/users/urls.py
+++ login/users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 
 from . import views
+from .views import get_name
 
 urlpatterns = [
     path('', views.UserListView.as_view()),
+    path('get_name/', get_name),
 ]
