51beb6b67527469bc683bc1f0b82efb39a495cdd
Gustavo Marques <gustavomarques.gml@gmail.com>
2018-10-14 11:34:23 -0300

    Creation of index page with version

--- login/login/views.py
+++ login/login/views.py
@@ -1,1 +1,16 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+import requests
 
+@api_view(['GET'])
+def status(request):
+    version=file_get_contents("../VERSION")
+    print (version)
+    return Response({
+    "name":"login-microservice",
+    "version":version,
+})
+
+def file_get_contents(filename):
+    with open(filename) as f:
+        return f.read()
