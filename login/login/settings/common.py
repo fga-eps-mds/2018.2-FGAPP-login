9073d0b92e3dfedbf69efb9212003e8837a521d1
Andre Lucas <andrelucax@gmail.com>
2018-11-02 20:38:41 -0300

    Added env-reference and changed email password to not show on config.

--- login/login/settings/common.py
+++ login/login/settings/common.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'integraappfga@gmail.com'
-EMAIL_HOST_PASSWORD = 'integrandoconhecimento'
+EMAIL_HOST_PASSWORD = config('SECURITY_EMAIL_PASSWORD', default='passwordShouldBeIn.Env')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
